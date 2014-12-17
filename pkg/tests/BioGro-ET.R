d7db2e76c648cb597b71fbe7ce3dc3d58c09a313
femiguez (Fernando Miguez) <femiguez@iastate.edu>
2014-12-17 16:14:24 +0000

    New test, lets see if it runs.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/biocro@64 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/tests/BioGro-ET.R
@@ -0,0 +1,19 @@
+## I will be testing the ET in BioCro
+## Date: Dec 17 2014
+
+data(cmi04)
+res <- BioGro(cmi04)
+
+cumET <- sum(res$CanopyTrans + res$SoilEvaporation) * 0.1
+
+if(cumET < 200 || cumET > 1000) stop("ET in BioGro is incorrect")
+
+maxStem <- max(res$Stem)
+maxLeaf <- max(res$Leaf)
+maxRoot <- max(res$Root)
+maxRhizome <- max(res$Rhizome)
+
+if(maxStem < 5) stop("maxStem in BioGro is incorrect")
+if(maxLeaf < 1) stop("maxLeaf in BioGro is incorrect")
+if(maxRoot < 0) stop("maxRoot in BioGro is incorrect")
+if(maxRhizome < 2) stop("maxRhizome in BioGro is incorrect")
